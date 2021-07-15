e9a2aba742ed77a9447787a1a623937ff6714b25
nono aroca <antonioaroca@correo.ugr.es>
2021-07-15 21:13:45 +0200

    YOLO a ver si ahora si

---
+++ yolo/prueba.py
@@ -0,0 +1,13 @@
+import torch
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+# Images
+imgs = ['https://ultralytics.com/images/zidane.jpg']  # batch of images
+
+# Inference
+results = model(imgs)
+
+# Results
+results.save()  # or .show()
