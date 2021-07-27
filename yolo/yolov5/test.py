e8bdd521b62329a1b7fa196be80b8467543f7170
nono-aroca <73640037+nono-aroca@users.noreply.github.com>
2021-07-27 17:23:13 +0200

    Add files via upload

--- yolo/yolov5/test.py
+++ yolo/yolov5/test.py
@@ -226,11 +226,15 @@ def run(data,
             stats.append((correct.cpu(), pred[:, 4].cpu(), pred[:, 5].cpu(), tcls))
 
         # Plot images
-        if plots and batch_i < 3:
-            f = save_dir / f'test_batch{batch_i}_labels.jpg'  # labels
-            Thread(target=plot_images, args=(img, targets, paths, f, names), daemon=True).start()
-            f = save_dir / f'test_batch{batch_i}_pred.jpg'  # predictions
-            Thread(target=plot_images, args=(img, output_to_target(out), paths, f, names), daemon=True).start()
+#        if plots and batch_i < 3:
+#            f = save_dir / f'test_batch{batch_i}_labels.jpg'  # labels
+#            Thread(target=plot_images, args=(img, targets, paths, f, names), daemon=True).start()
+#            f = save_dir / f'test_batch{batch_i}_pred.jpg'  # predictions
+#            Thread(target=plot_images, args=(img, output_to_target(out), paths, f, names), daemon=True).start()
+        f = save_dir / f'test_batch{batch_i}_labels.jpg'  # labels
+        Thread(target=plot_images, args=(img, targets, paths, f, names), daemon=True).start()
+        f = save_dir / f'test_batch{batch_i}_pred.jpg'  # predictions
+        Thread(target=plot_images, args=(img, output_to_target(out), paths, f, names), daemon=True).start()
 
     # Compute statistics
     stats = [np.concatenate(x, 0) for x in zip(*stats)]  # to numpy
